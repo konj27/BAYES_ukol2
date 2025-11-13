c6100190814200bbb4abcdf53fc967105d659477
Jan Konarik <j.konarik.photo@gmail.com>
2025-11-13 14:24:38 +0100

    making sure code is the same as in the book

--- reseniUkol2.R
+++ reseniUkol2.R
@@ -5,51 +5,62 @@ library(tidyverse)
 set.seed(69)
 n <- 30
 p_uspech <- 0.35
-real_array <- data.frame(mu = rbinom(5000,n,p_uspech))
+real_array <- data.frame(mu = rbinom(2000,n,p_uspech))
 ggplot(data = real_array, aes(x=mu))+
   geom_histogram(aes(y=..density..),color="white", bins=15)
 
 
-one_mh_iteration <- function(w, current, y, n, a, b){
+one_iteration <- function(a, b, current){
   # STEP 1: Propose the next chain location
-  proposal <- runif(1, min = current - w, max = current + w)
-  if(proposal <= 0 || proposal >= 1) proposal <- current
+  proposal <- rbeta(1, a, b)
   
   # STEP 2: Decide whether or not to go there
-  proposal_plaus <- dbeta(proposal, a, b) * dbinom(y, n, proposal)
-  current_plaus  <- dbeta(current, a, b) * dbinom(y, n, current)
-  alpha <- min(1, proposal_plaus / current_plaus)
+  proposal_plaus <- dbeta(proposal, 2, 3) * dbinom(1, 2, proposal)
+  proposal_q     <- dbeta(proposal, a, b)
+  current_plaus  <- dbeta(current, 2, 3) * dbinom(1, 2, current)
+  current_q      <- dbeta(current, a, b)
+  alpha <- min(1, proposal_plaus / current_plaus * current_q / proposal_q)
   next_stop <- sample(c(proposal, current), 
                       size = 1, prob = c(alpha, 1-alpha))
   
-  # Return the results
   return(data.frame(proposal, alpha, next_stop))
 }
-mh_tour <- function(N, w, y, n, a, b){
-  # 1. Start the chain at location 3
+betabin_tour <- function(N, a, b){
+  # 1. Start the chain at location 0.5
   current <- 0.5
-  chain <- numeric(N)
+  
+  # 2. Initialize the simulation
+  pi <- rep(0, N)
   
   # 3. Simulate N Markov chain stops
   for(i in 1:N){    
     # Simulate one iteration
-    sim <- one_mh_iteration(w = w, current = current, y, n, a, b)
+    sim <- one_iteration(a = a, b = b, current = current)
     
     # Record next location
-    chain[i] <- sim$next_stop
+    pi[i] <- sim$next_stop
     
     # Reset the current location
     current <- sim$next_stop
   }
   
   # 4. Return the chain locations
-  return(data.frame(iteration = c(1:N), p=chain))
+  return(data.frame(iteration = c(1:N), pi))
 }
 
 a <- 2; b <- 5; y <- real_array$mu
-mh_chain <- mh_tour(N = 5000, w = 0.1, y = y, n = n, a = a, b = b)
-mean(mh_chain$p)
-hist(mh_chain$p, breaks = 30, main = "Posterior of p", xlab = "p")
+betabin_sim <- betabin_tour(N = 5000, a = a, b = b)
+mean(betabin_sim$p)
+hist(betabin_sim$p, breaks = 30, main = "Posterior of p", xlab = "p")
+
+ggplot(betabin_sim, aes(x = iteration, y = pi)) + 
+  geom_line()
+ggplot(betabin_sim, aes(x = pi)) + 
+  geom_histogram(aes(y = ..density..), color = "white") + 
+  stat_function(fun = dbeta, args = list(3, 4), color = "blue")
+
 # 2 Jenda
+
+
 # 3 Anička
 # 4 Petr a Honza
